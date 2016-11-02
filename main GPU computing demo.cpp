8ef098f39ae8657b2fdd39c81510c9b5ad9f9aae
EliazarNelson <eliazarnelson@gmail.com>
2016-11-01 17:48:48 -0700

    Delete main GPU computing demo.cpp

--- main GPU computing demo.cpp
+++
@@ -1,9 +0,0 @@
-/*An experiment in OpenCL used AMD Radeon graphics card
-#include <iostream>
-#include <CL/cl.h>
-#include <stdio.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv) {
-	return 0;
-}
